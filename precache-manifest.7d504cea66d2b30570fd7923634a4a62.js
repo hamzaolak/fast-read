23c223bb95fd8b4e365dcd00b9a2656c3f624278
HAMZA OLAK <hamzaolak2@gmail.com>
2019-10-07 22:53:18 +0300

    Updates

---
+++ precache-manifest.7d504cea66d2b30570fd7923634a4a62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b52fa3f4e6fcb87e8ed7d71ffae249",
+    "url": "/fast-read-hamza/index.html"
+  },
+  {
+    "revision": "7c24deebfae323e91236",
+    "url": "/fast-read-hamza/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a8a693ed82972ae4ac4e",
+    "url": "/fast-read-hamza/static/js/2.0339c279.chunk.js"
+  },
+  {
+    "revision": "7c24deebfae323e91236",
+    "url": "/fast-read-hamza/static/js/main.dbadcf83.chunk.js"
+  },
+  {
+    "revision": "afc81e3207889b41d6ca",
+    "url": "/fast-read-hamza/static/js/runtime-main.e951605f.js"
+  }
+]);
--- static/js/main.dbadcf83.chunk.js
+++ static/js/main.dbadcf83.chunk.js
@@ -1,2 +1,2 @@
 (this["webpackJsonpfast-read"]=this["webpackJsonpfast-read"]||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},8:function(e,n,t){},9:function(e,n,t){"use strict";t.r(n);var o=t(0),a=t.n(o),r=t(2),i=t.n(r);t(8);var c=function(){return a.a.createElement("div",null,"Hello fast-read")};Boolean("localhost"===window.location.hostname||"[::1]"===window.location.hostname||window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/));i.a.render(a.a.createElement(c,null),document.getElementById("root")),"serviceWorker"in navigator&&navigator.serviceWorker.ready.then((function(e){e.unregister()}))}},[[3,1,2]]]);
-//# sourceMappingURL=main.78722e38.chunk.js.map
+//# sourceMappingURL=main.dbadcf83.chunk.js.map
