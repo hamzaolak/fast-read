7f7ecccf182906e4dac92334e9f2b32d589897af
jhui19 <jordanhui19@hotmail.com>
2019-10-14 14:29:51 -0400

    Changed names of colors

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,13 +1,3 @@
-/*export const colors = {
-    purpleColor: "#E1D5E7",
-    purpleDarkColor: "#9673A6",
-    tundora: "#4D4D4D",
-    codGray: "#1A1A1A",
-    chelseaCucumber: "#82B366",
-    alabasterapprox: "#FBFBFB",
-    white: "#FFFFFF"
-}*/
-
 export const colors = {
     primaryColor: "#E1D5E7",
     primaryDarkColor: "#9673A6",
