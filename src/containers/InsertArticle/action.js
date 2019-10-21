480c029f0c539206e50458a1ceb15eddebb20dc9
hamzaolak <hamzaolak2@gmail.com>
2019-10-21 21:13:09 +0000

    Add active Article and select with articleId

--- src/containers/InsertArticle/action.js
+++ src/containers/InsertArticle/action.js
@@ -6,7 +6,7 @@ export const insertAticle = (article, tooLongWord) => {
     const wordList = article.split(' ').map(word =>
         word.length < 30 ? word : tooLongWord)
     const numberOfPage = Math.ceil(wordList.length / 100)
-    const currentPage = 1
+    const currentPage = 0
 
     return {
         type: INSERT_ARTICLE,
--- src/containers/ReadArticle/index.js
+++ src/containers/ReadArticle/index.js
@@ -2,16 +2,25 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { useParams } from "react-router-dom";
 import { Step, StepList } from 'basic-react-steps'
+import { last, head, find } from 'lodash'
 
 import { PageWrapper } from 'components/Layouts'
+import Badge from 'components/Badge';
 
 const ReadArticle = ({ articleList }) => {
     const { articleId } = useParams();
+    const filteredList = find(articleList, function(article) { return article.id === articleId })
+    const article = filteredList ? head(filteredList) : last(articleList)
+    if (!article) window.location.href = '/'
+    const { numberOfPage, currentPage, wordList } = article
     return (
         <PageWrapper>
-            <StepList stepNumber={0}>
-
+            <StepList stepNumber={currentPage}>
+                {Array(numberOfPage).fill(<Step />)}
             </StepList>
+            {wordList.map( word => {
+                return <Badge color='#fbfcf1'>{word}</Badge>
+            })}
         </PageWrapper>
     )
 }
